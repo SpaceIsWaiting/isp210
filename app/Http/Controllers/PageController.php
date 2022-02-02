439e718f3bf52cc304dc507639bdadd35b7cbd8a
User0 <kirill_sapogov@list.ru>
2022-02-02 14:49:09 +0300

    Start laravel

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Lot;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+
+class PageController extends Controller
+{
+    public function index()
+    {
+        $categories = DB::table('categories')->get();
+        $lots = DB::table('lots')->orderBy("id", "desc")->take(6)->get();
+
+        return view('index', compact('lots', 'categories'));
+    }
+
+}
